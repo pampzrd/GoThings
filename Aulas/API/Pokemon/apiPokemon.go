7a9e3558fd76caa5bc9e58ca9d422166e08d836a
Pam Rod <akahime2014@gmail.com>
2024-02-04 02:15:22 -0300

    correcaoAPIpokemon

--- Aulas/API/Pokemon/apiPokemon.go
+++ Aulas/API/Pokemon/apiPokemon.go
@@ -19,17 +19,17 @@ import (
 
 // As estruturas vão indicar o que a função deve procurar no arquivo e atribuir para um campo.
 type Response struct {
-	nome    string    `json:"name"`
-	pokemon []pokemon `json:"pokemon_entries"`
+	Nome     string    `json:"name"`
+	Pokemons []Pokemon `json:"pokemon_entries"`
 }
 
-type pokemon struct {
-	numero  int            `json:"entry_number"`
-	especie pokemonSpecies `json:"pokemon_species"`
+type Pokemon struct {
+	Numero  int            `json:"entry_number"`
+	Especie PokemonSpecies `json:"pokemon_species"`
 }
 
-type pokemonSpecies struct {
-	nome string `json:"name"`
+type PokemonSpecies struct {
+	Nome string `json:"name"`
 }
 
 func main() {
@@ -45,17 +45,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(responseData)) // Precisa converter para string porque os dados brutos vem em bytes.
+	//fmt.Println(string(responseData)) // Converte para string porque os dados brutos vem em bytes(Porém não foi necessário). (Ela converteu aqui mas no exemplo não tem a conversão string aqui.)
 
-	var responseObject Response //Is not a type?   //Vai trabalhar com o responseData
+	var responseObject Response //Vai trabalhar com o responseData
 
 	json.Unmarshal(responseData, &responseObject) //Vai jogar os dados obtidos de responseData dentro de responseObject
 	//Imprime ?????
-	fmt.Println(responseObject.nome)
-	fmt.Println(len(responseObject.pokemon))
+	fmt.Println(responseObject.Nome)
+	fmt.Println(len(responseObject.Pokemons))
 
-	for _, pokemon := range responseObject.pokemon {
-		fmt.Println(pokemon.especie.nome)
+	for i := 0; i < len(responseObject.Pokemons); i++ {
+		fmt.Println(responseObject.Pokemons[i].Especie.Nome)
 	}
 
 }
+
+// NOTA : Deu o erro de reconhecimento com letras minúsculas nome de struct, campos e tipos.
+// NOTA : O resultado do exercício da professora retornava dados brutos porque ela converteu para string antes de chamar o retorno? Usando a conversão, não alterou o retorno.
---
+++ Aulas/API/Pokemon/go.mod
@@ -0,0 +1,3 @@
+module api/Pokemon
+
+go 1.21.5
